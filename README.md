c873a6d6a8ac7ba3fc9f97c89c3ffb9deeccbc40
Zbigniew Malcherczyk <zmalcherczyk@gmail.com>
2021-02-16 09:59:19 +0100

    PHPStan, ECS & PHPUnit config files (#1)
    "Kurła ile razy można konfigurować ecs'a" ~ Developer 12K
    "Dlaczego w każdym repozytorium trzeba pisać inaczej?" ~ Developer 14K
    
    Teraz można to sobie zaciągnąć do projektu, zrobić z tego libkę.. czy co tam będzie trzeba.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# php-coding-standards
+# Landingi Static Code Analyze Bundle
+
+This repository aggregates configuration files from libraries we are using in code testing
+
+## phpstan
+
+### packages
+* phpstan/phpstan
+* phpstan/phpstan-phpunit
+* phpstan/phpstan-symfony
+
+#### Optional
+* Jan0707/phpstan-prophecy - for Prophecy mocking tool
+* phpstan/phpstan-mockery
+
+## ecs
+
+### packages
+* symplify/easy-coding-standard
+
+## phpunit
+
+### packages
+* phpunit/phpunit
+* symfony/phpunit-bridge
+
+# Example Makefile
+
+```makefile
+ci:
+	bin/phpunit
+	vendor/bin/phpstan analyze -c phpstan.neon --memory-limit=256M
+	vendor/bin/ecs check src tests
+```
---
+++ ecs.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\FunctionNotation\SingleLineThrowFixer;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use PhpCsFixer\Fixer\Operator\IncrementStyleFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocAlignFixer;
+use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(OrderedImportsFixer::class)->call('configure', [
+        [
+            'importsOrder' => [
+                OrderedImportsFixer::IMPORT_TYPE_CONST,
+                OrderedImportsFixer::IMPORT_TYPE_CLASS,
+                OrderedImportsFixer::IMPORT_TYPE_FUNCTION
+            ]
+        ]
+    ]);
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $parameters->set(Option::SKIP, [
+        BlankLineAfterOpeningTagFixer::class => null,
+        NativeFunctionInvocationFixer::class => null,
+        ConcatSpaceFixer::class => null,
+        ClassAttributesSeparationFixer::class => null,
+        NoSuperfluousPhpdocTagsFixer::class => null,
+        IncrementStyleFixer::class => null,
+        BinaryOperatorSpacesFixer::class => null,
+        SingleLineThrowFixer::class => null,
+        PhpdocAlignFixer::class => null,
+    ]);
+    $parameters->set(Option::SETS, [
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+        SetList::SYMFONY,
+        SetList::SYMFONY_RISKY,
+    ]);
+};
