bba461907019a9b2f7fdd9f7632cb0cb989fb15b
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-10-19 13:16:14 +0200

    wtf? why it was twice

--- ecs.php
+++ ecs.php
@@ -35,7 +35,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     //Import
     $services->set(\PhpCsFixer\Fixer\Import\NoLeadingImportSlashFixer::class);
-    $services->set(\PhpCsFixer\Fixer\Import\NoUnusedImportsFixer::class);
     $services->set(\PhpCsFixer\Fixer\Import\SingleLineAfterImportsFixer::class);
     $services->set(\PhpCsFixer\Fixer\Import\NoUnusedImportsFixer::class);
     $services->set(\PhpCsFixer\Fixer\Import\OrderedImportsFixer::class)->call('configure', [['imports_order' => ['class', 'function', 'const']]]);
