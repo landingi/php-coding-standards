be109c30c91d58fb84e6e55ee26fb7f6fd3c8740
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-10-28 20:42:10 +0200

    New testing method and new classes for testing.

--- tests/FileNameSameAsClassNameTest.php
+++ tests/FileNameSameAsClassNameTest.php
@@ -13,77 +13,27 @@ class FileNameSameAsClassNameTest extends TestCase
     protected function setUp(): void
     {
         $this->fixer = new FileNameSameAsClassName();
-        $file = fopen("tests/test.php", "w");
-        fclose($file);
     }
 
     public function testDoesNotFix(): void
     {
-        $file = new \SplFileInfo('tests/test.php');
-        $tokens = Tokens::fromCode($this->getValidPhpCode());
+        $file = new \SplFileInfo(dirname(__DIR__) . '/data/test.php');
+        $tokens = Tokens::fromCode(file_get_contents(dirname(__DIR__) . '/data/test.php'));
         $this->fixer->fix($file, $tokens);
 
         static::assertEquals('test.php', $file->getBasename());
-        unlink('tests/test.php');
     }
 
     public function testDoesFix(): void
     {
-        $file = new \SplFileInfo('tests/test.php');
-        $tokens = Tokens::fromCode($this->getNotValidPhpCode());
+        $file = new \SplFileInfo(dirname(__DIR__) . '/data/changenametest.php');
+        $tokens = Tokens::fromCode(file_get_contents(dirname(__DIR__) . '/data/changenametest.php'));
         $this->fixer->fix($file, $tokens);
-        $file = new \SplFileInfo('tests/TestClass.php');
+        $file = new \SplFileInfo(dirname(__DIR__) . '/data/ChangeToThatName.php');
 
-        static::assertEquals('TestClass.php', $file->getBasename());
-        unlink('tests/TestClass.php');
-    }
+        static::assertEquals('ChangeToThatName.php', $file->getBasename());
 
-    public function testDoesFixWithFileWithSomeContent(): void
-    {
-        $file = new \SplFileInfo('tests/test.php');
-        $tokens = Tokens::fromCode($this->getNotValidPhpCodeWithContent());
-        $this->fixer->fix($file, $tokens);
-        $file = new \SplFileInfo('tests/TestClass.php');
-
-        static::assertEquals('TestClass.php', $file->getBasename());
-        unlink('tests/TestClass.php');
-    }
-
-    private function getValidPhpCode(): string
-    {
-        return <<<END
-        <?php
-
-        class test
-        {
-        
-        }
-        END;
-    }
-
-    private function getNotValidPhpCode(): string
-    {
-        return <<<END
-        <?php
-
-        class TestClass
-        {
-        
-        }
-        END;
-    }
-
-    private function getNotValidPhpCodeWithContent(): string
-    {
-        return <<<END
-        <?php
-
-        class TestClass
-        {
-        public function test(): string {
-            return 'test:)';
-         }
-        }
-        END;
+        $directoryPath = str_replace(basename($file->getPathname()), '', $file->getPathname());
+        rename($directoryPath.basename($file->getPathname()), $directoryPath.'changenametest.php');
     }
 }
