6d3489df8c782ed377fc4b3a42a47b1094525341
ferror <zmalcherczyk@gmail.com>
2021-11-03 18:08:59 +0100

    [PHP-250]Refactor fixer that changes Service and Entity suffix

--- src/ClassNameSuffixFixer.php
+++ src/ClassNameSuffixFixer.php
@@ -3,39 +3,76 @@ declare(strict_types=1);
 
 namespace Landingi;
 
-use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\Fixer\FixerInterface;
+use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
 use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
+use SplFileInfo;
 
-class ClassNameSuffixFixer extends AbstractFixer
+class ClassNameSuffixFixer implements FixerInterface
 {
-    protected function applyFix(\SplFileInfo $file, Tokens $tokens)
+    public function isCandidate(Tokens $tokens): bool
     {
         foreach ($tokens as $index => $token) {
-            if ($token->getContent() == 'class') {
+            if ($token->getContent() === 'class') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function isRisky(): bool
+    {
+        return false;
+    }
+
+    public function fix(SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token) {
+            if ($token->getContent() === 'class') {
                 $class = $tokens[$index + 2]->getContent();
 
-                if (str_contains($class, 'Service') && str_ends_with($class, 'Service')) {
-                    $newToken = str_replace("Service", "", $class);
-                    $tokens[$index + 2] = new Token([$index + 2, $newToken]);
-                }
-                if (str_contains($class, 'Entity') && str_ends_with($class, 'Entity')) {
-                    $newToken = str_replace("Entity", "", $class);
+                if (str_ends_with($class, 'Service') || str_ends_with($class, 'Entity')) {
+                    $newToken = str_replace(['Service', 'Entity'], '', $class);
                     $tokens[$index + 2] = new Token([$index + 2, $newToken]);
                 }
             }
         }
     }
 
-    public function isCandidate(Tokens $tokens): bool
-    {
-        return true;
-    }
-
     public function getDefinition(): FixerDefinitionInterface
     {
-        return new FixerDefinition('The suffix `Entity`, `Service` should be not used in class names.', [new \PhpCsFixer\FixerDefinition\CodeSample("<?php\n class AccountEntity")]);
+        $codeSample = <<<PHP
+<?php
+
+class PaymentService
+{
+}
+PHP;
+
+        return new FixerDefinition(
+            'Removes Service and Entity class suffix',
+            [
+                new CodeSample($codeSample)
+            ]
+        );
+    }
+
+    public function getName(): string
+    {
+        return 'class_name_suffix';
+    }
+
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
+    public function supports(SplFileInfo $file): bool
+    {
+        return true;
     }
 }
