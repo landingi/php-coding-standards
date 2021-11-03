71c0e8e93df1ae67463f1106aa9b493bfa4a0b3d
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-11-04 00:37:23 +0100

    Tests of interface fixer update.

--- tests/InterfaceNameSuffixFixerTest.php
+++ tests/InterfaceNameSuffixFixerTest.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace Landingi;
 
+use Generator;
 use PhpCsFixer\Tokenizer\Tokens;
 use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+use function file_get_contents;
 
 class InterfaceNameSuffixFixerTest extends TestCase
 {
@@ -15,63 +18,33 @@ class InterfaceNameSuffixFixerTest extends TestCase
         $this->fixer = new InterfaceNameSuffixFixer();
     }
 
-    public function testDoesNotFixAnything(): void
+    /**
+     * @dataProvider data
+     */
+    public function testDoesNotFixAnything(string $input, string $expected): void
     {
-        $tokens = Tokens::fromCode($this->getValidPhpCode());
-        $this->fixer->fix(new \SplFileInfo(''), $tokens);
+        $tokens = Tokens::fromCode($input);
+        $this->fixer->fix(new SplFileInfo(''), $tokens);
 
-        static::assertEquals($tokens->generateCode(), $this->getValidPhpCode());
+        self::assertEquals($tokens->generateCode(), $expected);
     }
 
-    public function testDoesFixInterfaceName(): void
+    /**
+     * yields [Code to Fix, Expected Result]
+     */
+    public function data(): Generator
     {
-        $tokens = Tokens::fromCode($this->getNotValidPhpCode());
-        $this->fixer->fix(new \SplFileInfo(''), $tokens);
-
-        static::assertEquals($tokens->generateCode(), $this->getValidPhpCode());
-    }
-
-    public function testDoesFixInterfaceNameWhichStartsWithInterfaceSuffix(): void
-    {
-        $tokens = Tokens::fromCode($this->getValidPhpCodeWithInterfaceOnStart());
-        $this->fixer->fix(new \SplFileInfo(''), $tokens);
-
-        static::assertEquals($tokens->generateCode(), $this->getValidPhpCodeWithInterfaceOnStart());
-    }
-
-    private function getValidPhpCode(): string
-    {
-        return <<<END
-        <?php
-
-        interface ExampleClass
-        {
-        
-        };
-        END;
-    }
-
-    private function getNotValidPhpCode(): string
-    {
-        return <<<END
-        <?php
-
-        interface ExampleClassInterface
-        {
-        
-        };
-        END;
-    }
-
-    private function getValidPhpCodeWithInterfaceOnStart(): string
-    {
-        return <<<END
-        <?php
-
-        interface InterfaceExampleClass
-        {
-        
-        };
-        END;
+        yield [
+            file_get_contents('tests/class_name_suffix_fixer/test_1_interface_input.php'),
+            file_get_contents('tests/class_name_suffix_fixer/test_1_interface_output.php'),
+        ];
+        yield [
+            file_get_contents('tests/class_name_suffix_fixer/test_2_interface_input.php'),
+            file_get_contents('tests/class_name_suffix_fixer/test_2_interface_output.php'),
+        ];
+        yield [
+            file_get_contents('tests/class_name_suffix_fixer/test_3_interface_input.php'),
+            file_get_contents('tests/class_name_suffix_fixer/test_3_interface_output.php'),
+        ];
     }
 }
